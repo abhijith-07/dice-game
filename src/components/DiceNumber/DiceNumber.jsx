11980cac99421e8c5c979b2293935e707df7f11d
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-03 10:56:00 +0530

    fix: dice number selection added

--- src/components/DiceNumber/DiceNumber.jsx
+++ src/components/DiceNumber/DiceNumber.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react"
 import styles from "./DiceNumber.module.css"
 
 export default function DiceNumber() {
+    
+    const [selectedNumber, setSelectedNumber] = useState()
     const diceNumber = [1, 2, 3, 4, 5, 6]
 
     return(
         <div className={styles.numberContainer}>
             <div className={styles.diceNumber}>
                 {diceNumber.map((value, i) => (
-                    <div className={styles.number} key={i}>{value}</div>
+                    <div className={styles.number} key={i} onClick={()=>{setSelectedNumber(value)}}>{value}</div>
                 ))}
             </div>
             <p>Select Number</p>
