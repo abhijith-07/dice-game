7a75a182b73c2d715cd097c90949f1bf51ef406e
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-03 11:09:36 +0530

    fix: onclick number color and bg change added

--- src/components/DiceNumber/DiceNumber.jsx
+++ src/components/DiceNumber/DiceNumber.jsx
@@ -1,19 +1,31 @@
-import { useState } from "react"
-import styles from "./DiceNumber.module.css"
+import { useState } from "react";
+import styles from "./DiceNumber.module.css";
 
 export default function DiceNumber() {
-    
-    const [selectedNumber, setSelectedNumber] = useState()
-    const diceNumber = [1, 2, 3, 4, 5, 6]
+  const [selectedNumber, setSelectedNumber] = useState();
+  const diceNumber = [1, 2, 3, 4, 5, 6];
 
-    return(
-        <div className={styles.numberContainer}>
-            <div className={styles.diceNumber}>
-                {diceNumber.map((value, i) => (
-                    <div className={styles.number} key={i} onClick={()=>{setSelectedNumber(value)}}>{value}</div>
-                ))}
-            </div>
-            <p>Select Number</p>
-        </div>
-    )
+  return (
+    <div className={styles.numberContainer}>
+      <div className={styles.diceNumber}>
+        {diceNumber.map((value, i) => (
+          <div
+            className={styles.number}
+            key={i}
+            onClick={() => {
+              setSelectedNumber(value);
+            }}
+            style={
+              selectedNumber === value
+                ? { background: "black", color: "white" }
+                : { background: "transparent", color: "black" }
+            }
+          >
+            {value}
+          </div>
+        ))}
+      </div>
+      <p>Select Number</p>
+    </div>
+  );
 }
