95614be09e97fb63e16912a5ba8a34b7747c0777
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-04 20:22:50 +0530

    fix: error message added

--- src/components/DiceNumber/DiceNumber.jsx
+++ src/components/DiceNumber/DiceNumber.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 import styles from "./DiceNumber.module.css";
 
-export default function DiceNumber() {
-  const [selectedNumber, setSelectedNumber] = useState();
+export default function DiceNumber({ selectedNumber, setSelectedNumber, errorMessage, setErrorMessage }) {
   const diceNumber = [1, 2, 3, 4, 5, 6];
+
+  function numberSelect(value) {
+    setSelectedNumber(value)
+    setErrorMessage("")
+  }
 
   return (
     <div className={styles.numberContainer}>
+      <div className={styles.errorMessage}>
+        {errorMessage}
+      </div>
       <div className={styles.diceNumber}>
         {diceNumber.map((value, i) => (
           <div
             className={styles.number}
             key={i}
             onClick={() => {
-              setSelectedNumber(value);
+              numberSelect(value);
             }}
             style={
               selectedNumber === value
