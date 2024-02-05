398cd3966a85fa44401ade4fa3fb6ae00615df76
Abhijith Subash <abhijithsubash07@gmail.com>
2024-02-05 12:56:59 +0530

    style: Tablet Responsive added

--- src/components/DiceNumber/DiceNumber.jsx
+++ src/components/DiceNumber/DiceNumber.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./DiceNumber.module.css";
 
 export default function DiceNumber({ selectedNumber, setSelectedNumber, errorMessage, setErrorMessage }) {
